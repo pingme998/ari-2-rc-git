ea06fd2ce68b90fa32a2920758489b0a6454250e
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-02-04 21:16:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-chmod +x /ari-2-rc-git/script/permission.sh
-bash /ari-2-rc-git/script/permission.sh
+chmod +x /ari-2-rc-git/script-plus-conf/permission.sh
+bash /ari-2-rc-git/script-plus-conf/permission.sh
 
-supervisord -c '/ari-2-rc-git/script/suoervisor.conf'
+supervisord -c '/ari-2-rc-git/script-plus-conf/supervisor.conf'
