f81c2753ebb60d10517f45cd4dbdc1519dc507af
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-02-06 23:27:40 +0530

    Update download.php

--- script-plus-conf/html/download.php
+++ script-plus-conf/html/download.php
@@ -1,12 +1,13 @@
 <?php
  $uri= 'url';
  $path = 'data.txt';
- if (isset($_POST['url']) && isset($_POST['thread'])) {
+ if (isset($_POST['url']) && isset($_POST['thread']) && isset($_POST['remote'])) {
     $fh = fopen($path,"a+");
-    $string = 'url="'. $_POST['url'].'" ;thread="'.$_POST['thread'].'"';
+    $string = 'url="'. $_POST['url'].'" ;thread="'.$_POST['thread'].'" ;remote="'.$_POST['remote'].'"';
     fwrite($fh,$string); 
-    fclose($fh); 
+    fclose($fh);
  }
+
 exec("cat /var/www/html/data.txt >>/ari-2-rc-git/script-plus-conf/urls.txt; echo >/var/www/html/data.txt");
 header('Location: index.html');
 
