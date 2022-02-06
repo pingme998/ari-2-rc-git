1e7017ecb6e8b49fd59670c7f3842e617d1a21f4
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-02-07 01:42:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ rm -r /var/www/html
 cp -r /ari-2-rc-git/script-plus-conf/html /var/www/
 curl -L '$config_in_url' >/ari-2-rc-git/script-plus-conf/rclone.conf
 chmod +x /ari-2-rc-git/script-plus-conf/nginx.sh
+cp /ari-2-rc-git/script-plus-conf/www.conf /etc/php/7.4/fpm/pool/
 echo $PORT >/PORT
 #/ari-2-rc-git/script-plus-conf/nginx.sh
 supervisord -c /ari-2-rc-git/script-plus-conf/supervisord.conf
