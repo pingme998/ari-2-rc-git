28c218f49c54eec299e5d7ad2692ded312a8aec2
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-02-07 16:58:19 +0530

    Update nginx.sh

--- script-plus-conf/nginx.sh
+++ script-plus-conf/nginx.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cat /ari-2-rc-git/script-plus-conf/default |sed "s/THERANDOMPORT/$(cat /PORT)/g" >/etc/nginx/sites-enabled/default
+#cat /ari-2-rc-git/script-plus-conf/default |sed "s/THERANDOMPORT/$(cat /PORT)/g" >/etc/nginx/sites-enabled/default
 nginx -g 'daemon off;'
