c0f064139579587da07ed334ddb16f46282cffe1
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-02-07 15:40:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,5 +11,21 @@ mkdir php/7.4/fpm
 mkdir php/7.4/fpm/pool
 cp /ari-2-rc-git/script-plus-conf/www.conf /etc/php/7.4/fpm/pool
 echo $PORT >/PORT
+
+
+# "/var/tmp/nginx" owned by "nginx" user is unusable on heroku dyno so re-create on runtime
+mkdir /var/tmp/nginx
+
+# make php-fpm be able to listen request from nginx (current user is nginx executor)
+sed -i -E "s/^;listen.owner = .*/listen.owner = $(whoami)/" /etc/php7/php-fpm.d/www.conf
+
+# make current user the executor of nginx and php-fpm expressly for local environment
+sed -i -E "s/^user = .*/user = $(whoami)/"  /etc/php/7.4/fpm/pool/www.conf
+sed -i -E "s/^group = (.*)/;group = \1/"  /etc/php/7.4/fpm/pool/www.conf
+sed -i -E "s/^user .*/user $(whoami);/" /etc/nginx/nginx.conf
+
+#supervisord --nodaemon --configuration /etc/supervisord.conf
 #/ari-2-rc-git/script-plus-conf/nginx.sh
+
+
 supervisord -c /ari-2-rc-git/script-plus-conf/supervisord.conf
