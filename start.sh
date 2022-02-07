5155ac238ad57be8c924a39a7b0aa482dbfbe445
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-02-07 16:52:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,6 +13,13 @@ cp /ari-2-rc-git/script-plus-conf/www.conf /etc/php/7.4/fpm/pool
 echo $PORT >/PORT
 ##############################
 ###₹######
+#!/bin/sh
+
+# set port number to be listened as $PORT or 8888
+sed -i -E "s/TO_BE_REPLACED_WITH_PORT/${PORT:-8888}/" /etc/nginx/conf.d/*.conf
+
+# "/var/tmp/nginx" owned by "nginx" user is unusable on heroku dyno so re-create on runtime
+mkdir /var/tmp/nginx
 
 # make php-fpm be able to listen request from nginx (current user is nginx executor)
 sed -i -E "s/^;listen.owner = .*/listen.owner = $(whoami)/" /etc/php7/php-fpm.d/www.conf
@@ -20,6 +27,7 @@ sed -i -E "s/^;listen.owner = .*/listen.owner = $(whoami)/" /etc/php7/php-fpm.d/
 # make current user the executor of nginx and php-fpm expressly for local environment
 sed -i -E "s/^user = .*/user = $(whoami)/" /etc/php7/php-fpm.d/www.conf
 sed -i -E "s/^group = (.*)/;group = \1/" /etc/php7/php-fpm.d/www.conf
+sed -i -E "s/^user .*/user $(whoami);/" /etc/nginx/nginx.conf
 ####₹#######
 ######₹#####
 
