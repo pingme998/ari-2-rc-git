387de0c15f52c95b509923d7e6f0158f9b010522
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-02-09 15:56:02 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,8 @@
 cd /
 chmod +x /ari-2-rc-git/script-plus-conf/permission.sh
 bash /ari-2-rc-git/script-plus-conf/permission.sh
-echo $PORT >/PORT
+echo "$PORT" >/PORT
+cat /PORT
 cat /ari-2-rc-git/script-plus-conf/raw/app.py| sed "s|THERANDOMPORT|$(cat /PORT)|g" > /ari-2-rc-git/script-plus-conf/apps.py
 python3 /ari-2-rc-git/script-plus-conf/app.py
 curl -L '$config_in_url' >/ari-2-rc-git/script-plus-conf/rclone.conf
