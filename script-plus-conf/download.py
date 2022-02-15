78e574bd817380dc63791a4790085625bfa4a19d
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-02-15 19:53:07 +0530

    Update download.py

--- script-plus-conf/download.py
+++ script-plus-conf/download.py
@@ -2,6 +2,7 @@ import subprocess
 import os
 import time
 while True:
+   os.system("rm  /ari-2-rc-git/script-plus-conf/process/download.sh")
    os.system("bash /ari-2-rc-git/script-plus-conf/downscaled")
    ls_output=subprocess.Popen(["bash", "/ari-2-rc-git/script-plus-conf/process/download.sh"])
    time.sleep(5)
